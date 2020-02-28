a117eb61de4c13db15664e51e6dcf6e65918d8f4
drmartirosian <55171534+drmartirosian@users.noreply.github.com>
2020-02-28 01:13:00 -0600

    Create, update blog

--- routes/articles.js
+++ routes/articles.js
@@ -1,10 +1,47 @@
 const router = require('express').Router();
 const Articles = require('../models/articles');
 
+//GET ALL ARTICLES REQUEST
 router.get('/', (req, res) => {
     Articles.find()
         .then(article => res.json(article))
-        .catch(err => res.status(400).res.json(`Error: ${err}`))
+        .catch(err => res.status(400).json(`Error: ${err}`))
 });
+
+//ADD NEW ARTICLE REQUEST
+router.post("/add", (req, res) => {
+    const newArticle = new Articles({
+        title: req.body.title,
+        article: req.body.article,
+        authorname: req.body.authorname
+    })
+    newArticle.save()
+        .then(()=> res.json("The new Article posted successfully!!!"))
+        .catch(err => res.status(400).json(`Error: ${err}`))
+});
+
+//FIND ARTICLE BY ID
+router.get('/:id', (req, res) => {
+    Articles.findById(req.params.id)
+    .then(article => res.json(article))
+    .catch(err => res.status(400).json(`Error: ${err}`))
+});
+
+//FIND ARTICLE BY ID AND UPDATE
+router.put('/update/:id', (req, res) => {
+    Articles.findById(req.params.id)
+    .then(article => {
+        article.title = req.body.title;
+        article.article = req.body.article;
+        article.authorname = req.body.authorname;
+
+        article
+            .save()
+            .then(()=> res.json("Article updated!"))
+            .catch(err => res.status(400).json(`Error: ${err}`))
+    })
+    .catch(err => res.status(400).json(`Error: ${err}`))
+})
+//FIND ARTICLE BY ID AND DELETE
 
 module.exports = router;
