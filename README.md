5053f187be3f550b4b5c56ea0619d8001651e9ec
drmartirosian <55171534+drmartirosian@users.noreply.github.com>
2020-02-27 22:45:53 -0600

    Boilerplate dropped

---
+++ README.md
@@ -0,0 +1,8 @@
+1. create .env file in root directory
+2. DATABASE_URL=`MONGODB` SECRET=`PASSWORD`
+3. npm i
+4. npm run build
+5. npm start
+
+
+The start of something big!
---
+++ src/pages/App/App.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import SignupPage from '../SignupPage/SignupPage';
+import LoginPage from '../LoginPage/LoginPage';
+import userService from '../../utils/userService';
+import HomePage from '../HomePage/HomePage';
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      // Initialize user if there's a token, otherwise null
+      user: userService.getUser()
+    };
+  }
+  handleLogout = () => {
+    userService.logout();
+    this.setState({ user: null });
+  }
+  handleSignupOrLogin = () => {
+    this.setState({user: userService.getUser()});
+  }
+
+  render() {
+
+    return (
+      <div>
+        <Switch>
+          <Route exact path='/' render={() =>
+            <HomePage
+              handleLogout={this.handleLogout}
+              user={this.state.user}
+            />
+          }/>
+          <Route exact path='/signup' render={({ history }) => 
+            <SignupPage
+              history={history}
+              handleSignupOrLogin={this.handleSignupOrLogin}
+            />
+          }/>
+          <Route exact path='/login' render={({ history }) => 
+            <LoginPage
+              history={history}
+              handleSignupOrLogin={this.handleSignupOrLogin}
+            />
+          }/>
+        </Switch>
+      </div>
+    );
+  }
+}
+
+export default App;
